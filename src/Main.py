2d3f5be0daa623630829c67518ea49edc294bf40
neibin.hu <adndcn@gmail.com>
2016-04-02 11:37:19 +0800

    Restruct the Code
    I restruct my  code, the code is according to lexical analyzer.

--- src/Main.py
+++ src/Main.py
@@ -1,51 +1,176 @@
-import re
-ctype = ['int', 'unsigned']
+import os
+import sys
 
-address = 0x36004
-space = 0
+        
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print 'lost the filename\n'
+        sys.exit()
+    elif len(sys.argv) > 2:
+        print 'too much arguments\n'
+        sys.exit()
+    file1 = open(sys.argv[1], 'rb')
+    filename = os.path.split(sys.argv[1])[-1]
+    filepath = os.path.split(sys.argv[1])[0]
+    file2 = open(filepath + '_' + sys.argv[1], 'wb')
+    
+    address = 0x0
+    pace = 0
+    
+    lines = list(file1)
+    processlines = []
+    word = ''
+    chnum = ''
+    flag = 0
+    bracketflag = 0
+    substructflag = 0
+    numflag = 0
+    preflag = 0
+    arrayflag = 0
+    num = 1
+    structname = ''
+    address = 0
+    space = 0
+    linenumber = 0
 
-file1 = open("teststruct.h",'rb')
-file2 = open('test.h', 'wb')
-
-lines = list(file1)
-processlines = []
-
-for line in lines:
-    temp = line.translate(None, '\t\n\r') #delete the tab, space and carrige return
-    if temp:
-        temp = temp.split(' ')
-        if temp[0] in ctype:
-            width = 4
-            num = 1
-            #check it is an array
-            if '[' in temp[-1]:
-                count = temp[-1].count('[')
-                patstring = r'.+' + '\[(.+)\]'*count
-                pattern = re.compile(patstring)
-                result = pattern.match(temp[-1])
+    for line in lines:
+        linenumber += 1
+        for i in range(len(line)):
+            if flag == 0: #nomal mode
+                if 'A' <= line[i] <= 'Z' or 'a' <= line[i] <= 'z':
+                    word += line[i]
+                elif line[i] == '/':
+                    if line[i+1] == '/':
+                        preflag = flag
+                        flag = 1 #comment mode
+                elif line[i] == ' ':
+                    if word == 'struct':
+                        substructflag = 1
+                        structname = line[:-2]
+                    word = '' #clear
+                elif line[i] == '{':
+                    if substructflag == 1:
+                        substructflag = 0
+                        flag = 2 #struct mode
+                        address = input('find a structure named: ' + structname + '\n' + 'please enter an address>')
+                        space = 0
+                    word = ''
+                else:
+                    word = ''
+                    
+                    
+            elif flag == 1: #comment mode
+                if line[i] == '\n':
+                    flag = preflag
                 
-                for i in range(count):
-                    num *= int(result.group(i+1))
-            address = address + space
-            space = width*num
-            #append the address at the end of the line (before \r\n).
-            pos = line.index('\r') #find the positon of \r in the string
-            line = line[:pos] + '   //0x' + str('%x' % address) + '\r\n'
-            file2.write(line)    
-        elif temp[0] == 'struct':
-            address = input('find a structure named: ' + temp[-1][:-1]+ '\n' + 'please enter an address>')
-            space = 0 
-            file2.write(line)
+            elif flag == 2: #struct mode
+                if 'A' <= line[i] <= 'Z' or 'a' <= line[i] <= 'z':
+                    word += line[i]
+                elif line[i] == '/':
+                    if line[i+1] == '/':
+                        preflag = flag
+                        flag = 1 #comment mode
+                elif line[i] == ' ':
+                    if word == 'char':
+                        numflag = 1
+                    elif word == 'short':
+                        numflag = 2
+                    elif word == 'int':
+                        numflag = 3
+                    word = ''
+                elif line[i] == '*':
+                    if word == 'char' or word == 'short' or word == 'int':
+                        print 'find pointer'
+                        numflag = 4
+                    elif word == '':
+                        if numflag > 0:
+                            numflag = 4
+                elif line[i] == ';':  #the end of one row
+                    address = address + space
+                    processlines.append({'linenumber': linenumber, 'addr': address})
+                    if numflag == 1:
+                        if arrayflag == 1:
+                            print 'find char array'
+                            space = num
+                        else:
+                            print 'find char'
+                            space = 1
+                    elif numflag == 2:
+                        if arrayflag == 1:
+                            print 'find short array'
+                            space = num*2
+                        else:
+                            print 'find short'
+                            space = 2
+                    elif numflag == 3:
+                        if arrayflag == 1:
+                            print 'find int array'
+                            space = num*4
+                        else:
+                            print 'find int'
+                            space = 4
+                    elif numflag == 4:
+                        if arrayflag == 1:
+                            print 'find pointer array'
+                            space = num*4
+                        else:
+                            print 'find pointer'
+                            space = 4
+                    numflag = 0
+                    bracketflag = 0
+                    word = ''
+                    arrayflag = 0
+                    num = 1
+                    break
+                elif line[i] == '[' and bracketflag == 0:
+                    bracketflag = 1
+                    arrayflag = 1
+                    chnum = ''
+                elif '0' <= line[i] <= '9' and bracketflag == 1:
+                    chnum += line[i]
+                elif line[i] == ']' and bracketflag == 1:
+                    num = num * int(chnum)
+                    bracketflag = 0
+                    chnum = ''
+                elif line[i] == '}':
+                        word = ''
+                        flag = 0
+                        bracketflag = 0
+                        substructflag = 0
+                        numflag = 0
+                        preflag = 0
+                        num = 1
+                        chnum = ''
+                        structname = ''
+                        address = 0
+                        arrayflag = 0
+    linenumber = 0
+    index = 0
+    maxindex = len(processlines)
+    for line2 in lines:
+        linenumber += 1
+        if index < maxindex:
+            if linenumber == processlines[index]['linenumber']:
+                if '/*' in line2:
+                    firstindex = line2.find('/*')
+                    secondindex = line2.find('*/')
+                    templine = line2[:firstindex] + str('/* 0x%05x ' % processlines[index]['addr']) + line2[secondindex:]
+                    file2.write(templine)
+                else:
+                    firstindex = line2.find('\r')
+                    templine = line2[:firstindex] + str('/* 0x%05x */' % processlines[index]['addr']) + line2[firstindex:]
+                    file2.write(templine)
+                index += 1
+            else:
+                file2.write(line2)
         else:
-            file2.write(line)
-    else:
-        file2.write(line)
-        
-file2.close()
-        
-
-# for line in processlines:
-#     temp = line.split(' ')
-#     if temp[0] in type:
-#         if
+            file2.write(line2)
+    
+    file2.close()
+    file1.close()                           
+                        
+            
+            
+            
+             
     
